ac55c52ae8305c498e1803244299a0665d5015f0
suresh766 <kumarsureshrock@gmail.com>
2022-02-01 20:25:25 +0530

    airport

--- src/test/java/org/one/FirestGittt.java
+++ src/test/java/org/one/FirestGittt.java
@@ -2,7 +2,7 @@ package org.one;
 
 public class FirestGittt {
 	public static void main(String[] args) {
-		System.out.println("okieeeeeee");
+		System.out.println("okieeeeeee---------------------------");
 	}
 
 }
